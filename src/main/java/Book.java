225d690d55a7c4275cfad3a0c79cb53a436f0872
ghr85 <ghrenfrew@gmail.com>
2019-03-26 17:09:49 +0000

    get title functionality added to book class

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,17 @@
 public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private Integer yearOfPublish;
+
+    public Book(String title, String author,String genre, Integer yearOfPublish){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.yearOfPublish = yearOfPublish;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,15 +1,17 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class BookTest {
     private Book book;
     @Before
     public void before(){
-        book = new Book("The iliad", "Homer",-750);
+        book = new Book("The iliad", "Homer","Classic",-750);
     }
     @Test
     public void hasTitle(){
-        assertEquals("The ilead", book.getTitle());
+        assertEquals("The iliad", book.getTitle());
     }
 
 }
